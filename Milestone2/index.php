fbe3a7584821969dd132e70d065bb99bf8650a58
katy01101011 <katy.91560@gmail.com>
2022-06-13 00:48:51 +0200

    Milestone2

--- Milestone2/index.php
+++ Milestone2/index.php
@@ -14,6 +14,8 @@ i dischi a php e li stamperà attraverso vue.
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Milestone2</title>
+    <!-- STYLE -->
+    <link rel="stylesheet" href="css/style.css">
     <!-- VUEJS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.7.0-alpha.6/vue.common.dev.min.js"></script>
     <!-- AXIOS -->
@@ -22,47 +24,30 @@ i dischi a php e li stamperà attraverso vue.
 </head>
 
 <body>
+    <!-- HEADER -->
+    <header>
+        <img src="img/logo-small.svg" alt="logo spotify">
+    </header>
+    <!-- /HEADER -->
 
-    <!DOCTYPE html>
-    <html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Milestone1</title>
-        <link rel="stylesheet" href="css/style.css">
-
-    </head>
-
-    <body>
-        <!-- HEADER -->
-        <header>
-            <img src="img/logo-small.svg" alt="logo spotify">
-        </header>
-        <!-- /HEADER -->
-
-        <!-- MAIN -->
-        <main id="app">
-            <div class="container">
-                <div v-for="card in database" class="card">
-                    <img :src="card.poster" alt="">
-                    <h2>
-                        {{ card.title }}
-                    </h2>
-                    <h3>
-                        {{ card.author }}
-                    </h3>
-                    <h4>
-                        {{ card.year }}
-                    </h4>
-                </div>
+    <!-- MAIN -->
+    <main id="app">
+        <div class="container">
+            <div v-for="card in database" class="card">
+                <img :src="card.poster" alt="">
+                <h2>
+                    {{ card.title }}
+                </h2>
+                <h3>
+                    {{ card.author }}
+                </h3>
+                <h4>
+                    {{ card.year }}
+                </h4>
             </div>
-        </main>
-        <!-- /MAIN -->
-    </body>
-
-    </html>
+        </div>
+    </main>
+    <!-- /MAIN -->
     <script src="js/script.js"></script>
 </body>
 
